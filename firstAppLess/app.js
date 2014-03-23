28ed4aeb9dbad4da9a246f955e4bb239a9c44134
anonhyme <antoine.bouchard2@usherbrooke.ca>
2014-03-22 21:16:15 -0400

    test page html

--- firstAppLess/app.js
+++ firstAppLess/app.js
@@ -14,13 +14,14 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hjs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
+app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
