0c434d9c18be438445379f4dd0d780b2ab6b2912
anonhyme <antoine.bouchard2@usherbrooke.ca>
2014-03-23 19:36:14 -0400

    update

---
+++ yoemen/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('yoemenApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
